46c337b277c83e76367aed7c4550ee24d888bc92
Luke Miloszewski <lukemiloszewski@gmail.com>
2022-02-19 16:45:16 +0200

    Add context module

---
+++ backend/src/ml_models/context.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+from typing import Any, Dict, Optional
+
+import onnxruntime
+
+
+_CONTEXT: Optional[Context] = None
+
+
+class Attributes:
+    def __init__(self, attributes_dict: Dict[str, Any]) -> None:
+        self._attributes_dict = attributes_dict
+
+    def __getitem__(self, attribute_id) -> Any:
+        rv = self._get_attribute(attribute_id=attribute_id)
+        return rv
+
+    def __getattr__(self, attribute_id) -> Any:
+        rv = self._get_attribute(attribute_id=attribute_id)
+        return rv
+
+    def _get_attribute(self, attribute_id: str) -> Any:
+        attribute = self._attributes_dict.get(attribute_id, None)
+        if attribute is None:
+            err_msg = f"Invalid attribute: {attribute_id}, options are: {self.list()}"
+            raise AttributeError(err_msg)
+        return attribute
+
+    def get(self, attribute_id: str, default=None) -> Any:
+        try:
+            rv = self._get_attribute(attribute_id=attribute_id)
+        except AttributeError:
+            rv = default
+        return rv
+
+    def list(self):
+        rv = self._attributes_dict.keys()
+        return rv
+
+
+class Context:
+    def __init__(self, resources: Attributes) -> None:
+        self.resources = resources
+
+
+def configure_context():
+    global _CONTEXT
+
+    mnist_model = 'model.onnx'
+    mnist_session = onnxruntime.InferenceSession(mnist_model, None)
+
+    attributes_dict = {
+        "mnist_session": mnist_session,
+    }
+
+    resources = Attributes(attributes_dict=attributes_dict)
+    context = Context(resources=resources)
+
+    _CONTEXT = context
+
+
+def get_context() -> Context:
+    if _CONTEXT is None:
+        raise ValueError("Context has not been initialised")
+    return _CONTEXT
