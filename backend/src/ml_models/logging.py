ff3ce6521a590fd7a880a34cf5fce49b7a47670a
Luke Miloszewski <lukemiloszewski@gmail.com>
2022-02-19 16:45:34 +0200

    Add logging module

---
+++ backend/src/ml_models/logging.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+
+
+_DEFAULT_LOG_FORMAT = "%(asctime)s|%(levelname)s|%(thread)d|%(module)s|%(message)s"
+
+
+def configure_logging():
+    console_formatter = logging.Formatter(_DEFAULT_LOG_FORMAT)
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(console_formatter)
+
+    logger = logging.getLogger()
+
+    logger.setLevel(logging.INFO)
+    logger.addHandler(console_handler)
