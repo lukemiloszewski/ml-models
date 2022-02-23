d6b9bb41c58a88be000e02db264c42e890abe17a
Luke Miloszewski <lukemiloszewski@gmail.com>
2022-02-23 21:32:06 +0200

    Add Container component

---
+++ frontend/src/components/Container/Container.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 60px 0; /* top-bottom left-right */
+  height: 100%;
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
--- frontend/src/pages/Mnist/Mnist.tsx
+++ frontend/src/pages/Mnist/Mnist.tsx
@@ -1,33 +1,37 @@
 import React, { useState } from "react";
+
 import CanvasDraw from "react-canvas-draw";
+
+import { Container } from "../../components/Container/Container";
 
 export function Mnist() {
   const [state, setState] = useState({
     loadTimeOffset: 0,
     lazyRadius: 0,
     brushRadius: 5,
-    brushColor: "red",
-    catenaryColor: "red",
-    hideGrid: false,
+    brushColor: "black",
+    hideGrid: true,
     canvasWidth: 300,
     canvasHeight: 300,
-    hideInterface: false,
+    hideInterface: true,
   });
   const [saveableCanvas, setSaveableCanvas] = useState<CanvasDraw | null>(null);
 
   return (
-    <div>
-      <CanvasDraw
-        {...state}
-        ref={(canvasDraw: CanvasDraw) => setSaveableCanvas(canvasDraw)}
-      />
-      <button
-        onClick={() => {
-          saveableCanvas?.clear();
-        }}
-      >
-        Clear
-      </button>
-    </div>
+    <Container>
+      <div>
+        <CanvasDraw
+          {...state}
+          ref={(canvasDraw: CanvasDraw) => setSaveableCanvas(canvasDraw)}
+        />
+        <button
+          onClick={() => {
+            saveableCanvas?.clear();
+          }}
+        >
+          Clear
+        </button>
+      </div>
+    </Container>
   );
 }
