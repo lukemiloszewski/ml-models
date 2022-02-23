2d6f993925708fb16af4a8e8ace00d74ccbf4a1d
Luke Miloszewski <lukemiloszewski@gmail.com>
2022-02-23 21:17:42 +0200

    Reorganise App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,13 @@
-import React, { useState } from "react";
-import CanvasDraw from "react-canvas-draw";
+import React from "react";
 
-function App() {
-  const [state, setState] = useState({
-    loadTimeOffset: 0,
-    lazyRadius: 0,
-    brushRadius: 5,
-    brushColor: "red",
-    catenaryColor: "red",
-    hideGrid: false,
-    canvasWidth: 300,
-    canvasHeight: 300,
-    hideInterface: false,
-  });
-  const [saveableCanvas, setSaveableCanvas] = useState<CanvasDraw | null>(null);
+import { GlobalStyle } from "./components/";
+import { Mnist } from "./pages/";
 
+export function App() {
   return (
-    <div>
-      <CanvasDraw
-        {...state}
-        ref={(canvasDraw: CanvasDraw) => setSaveableCanvas(canvasDraw)}
-      />
-      <button
-        onClick={() => {
-          saveableCanvas?.clear();
-        }}
-      >
-        Clear
-      </button>
-    </div>
+    <React.Fragment>
+      <GlobalStyle />
+      <Mnist />
+    </React.Fragment>
   );
 }
-
-export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { GlobalStyle } from "./components/";
-import App from "./App";
+import { App } from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
