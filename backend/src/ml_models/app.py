de8599a2f56b480d412242b5356730c202fbe4a4
Luke Miloszewski <lukemiloszewski@gmail.com>
2022-02-24 22:33:16 +0200

    Add cors origin

--- backend/src/ml_models/app.py
+++ backend/src/ml_models/app.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from ml_models import __DESCRIPTION__, __TITLE__, __VERSION__
 from ml_models.routers import health_router, mnist_router
+
+
+_origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
 
 
 def configure_app() -> FastAPI:
@@ -11,6 +18,14 @@ def configure_app() -> FastAPI:
         version=__VERSION__,
         openapi_tags=[health_router.tag, mnist_router.tag],
     )
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+)
 
     app.include_router(health_router.router, prefix=health_router.prefix)
     app.include_router(mnist_router.router, prefix=mnist_router.prefix)
