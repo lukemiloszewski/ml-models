ec57ba855d41d49e8960f480c7ea10a3aeae3ec7
Luke Miloszewski <lukemiloszewski@gmail.com>
2022-02-27 12:27:36 +0200

    Create app script uses config

--- backend/scripts/main.py
+++ backend/scripts/main.py
@@ -1,8 +1,9 @@
 import uvicorn
 
+from ml_models import config
 from ml_models.bootstrap import create_app
 
-app = create_app()
+app = create_app(config)
 
 if __name__ == "__main__":
     uvicorn.run(app=app, use_colors=True)
