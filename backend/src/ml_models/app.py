6c9f81cee59158a6e63fc6a27613cda00c3c1e63
Luke Miloszewski <lukemiloszewski@gmail.com>
2022-03-12 17:06:54 +0200

    Fix logging namespace

--- backend/src/ml_models/app.py
+++ backend/src/ml_models/app.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from ml_models import __DESCRIPTION__, __TITLE__, __VERSION__
-from ml_models.log import log_request_metadata
+from ml_models.logging import log_request_metadata
 from ml_models.routers import health_router, mnist_router
 
 
