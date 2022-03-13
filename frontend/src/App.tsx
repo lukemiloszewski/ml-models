636bc8526e59ac5c87fc830c84c60e459c1976df
Luke Miloszewski <lukemiloszewski@gmail.com>
2022-03-13 10:53:34 +0200

    Wrap app with QueryClientProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import React from "react";
 
 import { GlobalStyle } from "./components/";
 import { Mnist } from "./pages/";
 
+const queryClient = new QueryClient();
+
 export function App() {
   return (
-    <React.Fragment>
-      <GlobalStyle />
-      <Mnist />
-    </React.Fragment>
+    <QueryClientProvider client={queryClient}>
+      <React.Fragment>
+        <GlobalStyle />
+        <Mnist />
+      </React.Fragment>
+    </QueryClientProvider>
   );
 }
