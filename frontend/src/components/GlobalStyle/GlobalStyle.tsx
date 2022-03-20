150a260610c47f63fa4fb31e4c6d418abeb9c421
Luke Miloszewski <lukemiloszewski@gmail.com>
2022-03-20 08:38:57 +0200

    New cool background

--- frontend/src/components/GlobalStyle/GlobalStyle.tsx
+++ frontend/src/components/GlobalStyle/GlobalStyle.tsx
@@ -2,8 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   body {
-    background: rgb(134,39,166);
-    background: linear-gradient(90deg, rgba(134,39,166,1) 0%, rgba(244,75,75,1) 50%, rgba(252,176,69,1) 100%);
+    background: #fafafa;
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
     "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
