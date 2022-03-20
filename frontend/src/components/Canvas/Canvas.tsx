c929f16888dbba5f89d76c8cdf1a16185bf60e83
Luke Miloszewski <lukemiloszewski@gmail.com>
2022-03-20 08:38:15 +0200

    Transition in and out

--- frontend/src/components/Canvas/Canvas.tsx
+++ frontend/src/components/Canvas/Canvas.tsx
@@ -3,8 +3,12 @@ import styled from "styled-components";
 import CanvasDraw from "react-canvas-draw";
 
 export const Canvas = styled(CanvasDraw)`
-  border: 5px solid black;
+  border: 5px solid rgb(191, 216, 252);
+  border-radius: 12px;
+  transition: 0.3s;
   :hover {
     cursor: pointer;
+    border-color: rgb(142, 186, 248);
+    transition: 0.3s;
   }
 `;
