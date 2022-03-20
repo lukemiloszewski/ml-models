186e56f0eda7b117988c09cf8272ffff3aa33318
Luke Miloszewski <lukemiloszewski@gmail.com>
2022-03-20 08:38:47 +0200

    Fix exports

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -1,8 +1,15 @@
-import { Button } from "./Button/Button";
+import { StyledButton } from "./Button/Button";
 import { Canvas } from "./Canvas/Canvas";
 import { Container } from "./Container/Container";
 import { GlobalStyle } from "./GlobalStyle/GlobalStyle";
 import { Prediction } from "./Prediction/Prediction";
 import { ButtonGroup } from "./ButtonGroup/ButtonGroup";
 
-export { Button, ButtonGroup, Canvas, Container, GlobalStyle, Prediction };
+export {
+  StyledButton,
+  ButtonGroup,
+  Canvas,
+  Container,
+  GlobalStyle,
+  Prediction,
+};
